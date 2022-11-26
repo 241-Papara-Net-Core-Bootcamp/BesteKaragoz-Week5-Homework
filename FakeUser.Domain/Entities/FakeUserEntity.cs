abe3101508a8e631527ac2c16413eb828271f2d2
Beste Karagöz <30674420+bestekrgz@users.noreply.github.com>
2022-11-26 17:52:26 +0300

    Add files via upload

---
+++ FakeUser.Domain/Entities/FakeUserEntity.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace FakeUser.Domain.Entities
+{
+    public class FakeUserEntity : BaseEntity
+    {
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
